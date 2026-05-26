68ac9eb715b85945af716bde46282536a0e2a32a
Eyerusalem T/brhan <4u.aplus.tutor@gmail.com>
2026-05-26 13:11:25 +0300

    Add files via upload

---
+++ nest-app/src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProjectsModule } from './projects/projects.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { TasksModule } from './tasks/tasks.module';
+
+@Module({
+  imports: [ProjectsModule, UsersModule, AuthModule, TasksModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
